0dee2717e238667543c9a29e99d4302d840a2e01
Francisco Costa <francisco@fcosta.pt>
2019-06-26 14:23:08 +0100

    PLayers and total balance of lottery showing on frontend.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import lottery from "./lottery";
 export default class App extends Component {
   state = {
     manager: "",
-    players: ""
+    players: [],
+    balance: ""
   };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.returnPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address); // In wei
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -20,7 +23,15 @@ export default class App extends Component {
       <div>
         <h2>Lottery Contract</h2>
         <p>This contract is managed by {this.state.manager}</p>
-        <p>There are currently {this.state.players.length} in the lottery</p>
+        <p>
+          There are currently {this.state.players.length} players in the
+          lottery.
+        </p>
+        <p>
+          {" "}
+          Currently, the lottery has{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether.
+        </p>
       </div>
     );
   }
